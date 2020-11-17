acb1b3ec1451062dc16ee1e40ccc106b948c6d9d
whollybrewed <andylee47695@gmail.com>
2020-11-17 04:40:14 +0100

    add chi square test

--- assignment_1/robot_test.m
+++ assignment_1/robot_test.m
@@ -1,7 +1,7 @@
 clearvars;clc;
 Robot=load('AssignmentPredictiveMaintenance.mat');
-speed=Robot.RobotArms(4).speed;
-time=Robot.RobotArms(4).settling_time;
+speed=Robot.RobotArms(19).speed;
+time=Robot.RobotArms(19).settling_time;
 invalid_speed=speed~=4.2;
 time(invalid_speed)=[];
 N=length(time);
@@ -16,17 +16,22 @@ s_sigma=sqrt(s_var);
 s_z = sqrt(N).*(s_mean-mu)/s_sigma;
 % p-value
 pval=cdf("Normal",-abs(s_z),0,1)+(1-cdf("Normal",abs(s_z),0,1));
-% convert 1-alph to x-axis 
-z_half=abs(icdf('Normal',half_alpha,0,1));
+% half width  
+half_width=abs(icdf('Normal',half_alpha,0,1)).*s_sigma/sqrt(N);
 % confidenc interval
-ci_l=s_mean-z_half.*s_sigma/sqrt(N);
-ci_r=s_mean+z_half.*s_sigma/sqrt(N);
+ci_l=s_mean-half_width;
+ci_r=s_mean+half_width;
+%chi squared testing
+bin = 100;
+x = norminv(1/bin:1/bin:(bin-1)/bin,mu,sigma);
+pd = makedist('Normal','mu',mu,'sigma',sigma);
+y=diff(cdf(pd,x));
+y=[(1-sum(y))/2 y (1-sum(y))/2];
+expcount=N*y;
+edges=[-inf x inf];
+bincount=histcounts(time,edges);
+chisquare=sum(((bincount-expcount).^2)./expcount);
+icdf("Chisquare",0.99,bin-1);
 
-% p = normspec([-abs(s_z),abs(s_z)],0,1,'outside');
-% p = [0.025,0.975];
-% ci2 = icdf('Normal',0.025,0,1);
-% normspec(pval,0,1,'outside');
-% [h,pvalue,ci] = ztest(v_time,mu,sigma);
-% y=exp(-0.5*((x-0)/1).^2)/(1*sqrt(2*pi));
 
    
